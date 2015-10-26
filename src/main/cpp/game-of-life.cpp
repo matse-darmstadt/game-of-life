def47777be5a8dc79239bdfb64055b92a843a18b
jembel <j.belmokhtar@mgsoftech.com>
2015-10-26 16:05:55 +0100

    added sha1 class + base64 encoding; unit tests are included and run successfully

--- src/main/cpp/game-of-life.cpp
+++ src/main/cpp/game-of-life.cpp
@@ -7,66 +7,22 @@
  ============================================================================
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
+#include "Server.h"
+#include <iostream>
 
-void error(const char *msg) {
-	perror(msg);
-	exit(1);
-}
+const int PORT_TEST = 1234;
+
+using namespace std;
 
 int main(int argc, char *argv[]) {
-	int sockfd, newsockfd;
-	socklen_t clilen;
-	char buffer[256];
-	struct sockaddr_in serv_addr, cli_addr;
 
-	sockfd = socket(AF_INET, SOCK_STREAM, 0);
-	if (sockfd < 0) {
-		error("ERROR opening socket");
-	}
 
-	/*
-	 * Tell Linux that we want to listen to a TCP socket with the port number 1234
-	 * and any IP address
-	 */
-	bzero((char *) &serv_addr, sizeof(serv_addr));
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_addr.s_addr = INADDR_ANY;
-	serv_addr.sin_port = htons(1234); // The port the server will be listening to
-	if (bind(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0) {
-		error("ERROR on binding");
-	}
-	while (true) {
-		listen(sockfd, 5);
+	Server srv(PORT_TEST);
 
-		clilen = sizeof(cli_addr);
-		newsockfd = accept(sockfd, (struct sockaddr *) &cli_addr, &clilen);
-		if (newsockfd < 0) {
-			error("ERROR on accept");
-		}
+	srv.test();
 
-		// Receive some message up to 255 Bytes (Byte 255 stays '\0')
-		bzero(buffer, 256);
-		int n = read(newsockfd, buffer, 255);
-		if (n < 0) {
-			error("ERROR reading from socket");
-		}
-		printf("Server received: %s\n", buffer);
+	srv.start();
 
-		// Response to the message
-		n = write(newsockfd, "Some message", 12);
-		if (n < 0) {
-			error("ERROR writing to socket");
-		}
-		close(newsockfd);
-	}
 
-	close(sockfd);
 	return 0;
 }
